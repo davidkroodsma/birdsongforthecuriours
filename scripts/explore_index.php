a0464c0ec2cbe3a2689c4251e935a9af3cadad0d
David Kroodsma <davidk@skytruth.org>
2019-12-15 13:27:30 -0800

    updated explore index

---
+++ scripts/explore_index.php
@@ -0,0 +1,101 @@
+<li><a href="#"><i class="fas fa-plus-circle"></i> Explore 1-10</a>
+						<ul class="show-hide">							<li><a href="explore.php?page=1">1.  Chick-a-dee calls of North American chickadees.</a></li>
+							<li><a href="explore.php?page=2">2.  White-breasted nuthatch songs.</a></li>
+							<li><a href="explore.php?page=3">3.  American robin songs.</a></li>
+							<li><a href="explore.php?page=4">4.  Mallard quacks and rab-rabs.</a></li>
+							<li><a href="explore.php?page=5">5.  When and why do chimney swifts twitter?</a></li>
+							<li><a href="explore.php?page=6">6.  Red-shouldered hawk counting games.</a></li>
+							<li><a href="explore.php?page=7">7.  Duets by Carolina wrens and wrentits.</a></li>
+							<li><a href="explore.php?page=8">8.  Listening to female red-winged blackbirds.</a></li>
+							<li><a href="explore.php?page=9">9.  Singing from the nest by cardinals and grosbeaks.</a></li>
+							<li><a href="explore.php?page=10">10.  Whistling wings of mourning doves.</a></li>
+						</ul>
+					</li>
+<li><a href="#"><i class="fas fa-plus-circle"></i> Explore 11-20</a>
+						<ul class="show-hide">							<li><a href="explore.php?page=11">11.  Peenting and VROOMing common nighthawks.</a></li>
+							<li><a href="explore.php?page=12">12.  Winnows of Wilson’s snipe.</a></li>
+							<li><a href="explore.php?page=13">13.  The “double-strike” of sapsuckers.</a></li>
+							<li><a href="explore.php?page=14">14.  Indefatigable, bachelor males advertising for a mate.</a></li>
+							<li><a href="explore.php?page=15">15.  Singing for more and more mates.</a></li>
+							<li><a href="explore.php?page=16">16.  Listening for courtship songs.</a></li>
+							<li><a href="explore.php?page=17">17.  The puzzling case of the California thrasher.</a></li>
+							<li><a href="explore.php?page=18">18.  Bird brains and special song control neurons.</a></li>
+							<li><a href="explore.php?page=19">19.  Memories of birds and human listeners.</a></li>
+							<li><a href="explore.php?page=20">20.  Seeing the two voices of birds in sonagrams.</a></li>
+						</ul>
+					</li>
+<li><a href="#"><i class="fas fa-plus-circle"></i> Explore 21-30</a>
+						<ul class="show-hide">							<li><a href="explore.php?page=21">21.  Playing songs at slower speeds, lower frequencies.</a></li>
+							<li><a href="explore.php?page=22">22.  No dialects in the innate songs of flycatchers, doves, etc. .</a></li>
+							<li><a href="explore.php?page=23">23.  Improvised songs of red-eyed vireos.</a></li>
+							<li><a href="explore.php?page=24">24.  The practice singing of young songbirds.</a></li>
+							<li><a href="explore.php?page=25">25.  Listening for practice singing during spring.</a></li>
+							<li><a href="explore.php?page=26">26.  Neighboring male songbirds learn from each other.</a></li>
+							<li><a href="explore.php?page=27">27.  Call matching by ravens, jays, and other corvids.</a></li>
+							<li><a href="explore.php?page=28">28.  Song matching by neighboring western male marsh wrens.</a></li>
+							<li><a href="explore.php?page=29">29.  Song matching by titmice.</a></li>
+							<li><a href="explore.php?page=30">30.  Dialects in black-capped chickadee songs.</a></li>
+						</ul>
+					</li>
+<li><a href="#"><i class="fas fa-plus-circle"></i> Explore 31-40</a>
+						<ul class="show-hide">							<li><a href="explore.php?page=31">31.  Bicycling among dickcissel song dialects.</a></li>
+							<li><a href="explore.php?page=32">32.  A road-trip through white-throated sparrow song dialects.</a></li>
+							<li><a href="explore.php?page=33">33.  Call matching by male red-winged blackbirds.</a></li>
+							<li><a href="explore.php?page=34">34.  Mockingbird mimicry.</a></li>
+							<li><a href="explore.php?page=35">35.  Tuning your ears to hear mimicry.</a></li>
+							<li><a href="explore.php?page=36">36.  Scaling song complexity.</a></li>
+							<li><a href="explore.php?page=37">37.  How phoebes use their two to three songs.</a></li>
+							<li><a href="explore.php?page=38">38.  Songs of ruby-crowned kinglets.</a></li>
+							<li><a href="explore.php?page=39">39.  Seeing the magnificence of hermit thrush songs.</a></li>
+							<li><a href="explore.php?page=40">40.  Getting to know song repertoires of individual birds.</a></li>
+						</ul>
+					</li>
+<li><a href="#"><i class="fas fa-plus-circle"></i> Explore 41-50</a>
+						<ul class="show-hide">							<li><a href="explore.php?page=41">41.  Patience in listening to songs of a brown thrasher.</a></li>
+							<li><a href="explore.php?page=42">42.  Once you are attuned, every singing bird becomes interesting.</a></li>
+							<li><a href="explore.php?page=43">43.  Song packages of the Philadelphia vireo.</a></li>
+							<li><a href="explore.php?page=44">44.  Listening for chat song packages.</a></li>
+							<li><a href="explore.php?page=45">45.  Song packages of yellow-throated vireos.</a></li>
+							<li><a href="explore.php?page=46">46.  Listening for patterns in how songbirds present their song repertoires.</a></li>
+							<li><a href="explore.php?page=47">47. What do songless songbirds do during the dawn chorus?</a></li>
+							<li><a href="explore.php?page=48">48. Dawn calling choruses in Swainson’s thrushes and veeries.</a></li>
+							<li><a href="explore.php?page=49">49. Mockingbird calls at dusk and dawn.</a></li>
+							<li><a href="explore.php?page=50">50. Cardinals awaking—13 mornings to explore!</a></li>
+						</ul>
+					</li>
+<li><a href="#"><i class="fas fa-plus-circle"></i> Explore 51-60</a>
+						<ul class="show-hide">							<li><a href="explore.php?page=51">51. Exploring horned lark singing with Raven Lite.</a></li>
+							<li><a href="explore.php?page=52">52. Warbler dawn choruses are special.</a></li>
+							<li><a href="explore.php?page=53">53. Individuality among red-winged blackbirds.</a></li>
+							<li><a href="explore.php?page=54">54. An extreme blackbird challenge for you!</a></li>
+							<li><a href="explore.php?page=55">55. The thrill of dawn-singing Brewer’s sparrows.</a></li>
+							<li><a href="explore.php?page=56">56. You can unravel the case of clay-colored sparrows.</a></li>
+							<li><a href="explore.php?page=57">57. Choose your own dawn chorus experience.</a></li>
+							<li><a href="explore.php?page=58">58. Night singers!</a></li>
+							<li><a href="explore.php?page=59">59. Owling.</a></li>
+							<li><a href="explore.php?page=60">60. Purple martin song flights.</a></li>
+						</ul>
+					</li>
+<li><a href="#"><i class="fas fa-plus-circle"></i> Explore 61-70</a>
+						<ul class="show-hide">							<li><a href="explore.php?page=61">61. Sage thrasher songs, through the night.</a></li>
+							<li><a href="explore.php?page=62">62. Flight songs of ovenbirds.</a></li>
+							<li><a href="explore.php?page=63">63. Get to know the American woodcock.</a></li>
+							<li><a href="explore.php?page=64">64. Dawn song flights of tree swallows.</a></li>
+							<li><a href="explore.php?page=65">65. Prairie flight songs. </a></li>
+							<li><a href="explore.php?page=66">66. Just when do birds sing in flight?</a></li>
+							<li><a href="explore.php?page=67">67. Recognizing individuals by their songs.</a></li>
+							<li><a href="explore.php?page=68">68. Discovering new species by their songs.</a></li>
+							<li><a href="explore.php?page=69">69. Just what is a “species”?</a></li>
+							<li><a href="explore.php?page=70">70. The joy of western meadowlark songs.</a></li>
+						</ul>
+					</li>
+<li><a href="#"><i class="fas fa-plus-circle"></i> Explore 71-77</a>
+						<ul class="show-hide">							<li><a href="explore.php?page=71">71. Song changes over evolutionary time.</a></li>
+							<li><a href="explore.php?page=72">72. The curious case of pitch-shifting black-capped chickadees.</a></li>
+							<li><a href="explore.php?page=73">73. Themes and variations in birdsong.</a></li>
+							<li><a href="explore.php?page=74">74. Exploring birdsong with human musicians.</a></li>
+							<li><a href="explore.php?page=75">75. Birdsong and musicality. </a></li>
+							<li><a href="explore.php?page=76">76. Get to know this wren!</a></li>
+							<li><a href="explore.php?page=77">77. Develop your own list of favored musicians.</a></li>
+						</ul>
+					</li>
