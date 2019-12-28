60907742f30721f1c2bebd107427bea3f4d6b22d
David Kroodsma <davidk@skytruth.org>
2019-12-27 18:27:20 -0800

    updated buy and header.php

---
+++ header.php
@@ -0,0 +1,6 @@
+				<li><a href="index.php">Home</a></li>
+				<li><a href="search.php">Listen &amp; Explore</a></li>
+				<li><a href="page.php" target="_blank">Look Inside</a></li>
+				<li><a href="learn-more.php">Learn More</a></li>
+				<!--<li><a href="page.php">Downloads</a></li>-->
+				<li><a href="#buy-popup" class="open-modal" rel="modal:open">Buy the Book</a></li>
