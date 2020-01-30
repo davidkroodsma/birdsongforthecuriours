9e36cf9e653d2fa67a0483859c85564df86df753
David Kroodsma <davidk@skytruth.org>
2020-01-30 06:00:07 -0800

    fixed explore play issue, explore 50 table, buy link for powels

--- buy-links.php
+++ buy-links.php
@@ -1,4 +1,4 @@
 					<li><a href="https://www.barnesandnoble.com/w/birdsong-for-the-curious-naturalist-donald-kroodsma/1132304327?ean=9781328919113&st=PLA&sid=BNB_ADL+Core+Generic+Books+-+Desktop+Medium&sourceId=PLAGoNA&dpid=tdtve346c&2sid=Google_c&gclid=Cj0KCQiAuefvBRDXARIsAFEOQ9FVucYZ54Er5pO_PsxqgOSNs-agI7rpfBvlonM3nLGeZ496vrleLz4aAtbkEALw_wcB">Barnes &amp; Noble</a></li>
 					<li><a href="https://www.indiebound.org/search/book?keys=birdsong+for+the+curious+naturalist">Indie</a></li>
 					<li><a href="https://www.amazon.com/gp/product/B07SZBFXR9?pf_rd_p=ab873d20-a0ca-439b-ac45-cd78f07a84d8&pf_rd_r=Y8DS8WQXNDQEMXQZT9NT">Amazon</a></li>
-					<li><a href="https://www.powells.com/searchresults?keyword=birdsong+for+the+curious+naturalist">Powell's</a></li>
+					<li><a href="https://www.powells.com/book/-9781328919113">Powell's</a></li>
